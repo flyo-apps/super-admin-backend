902ce94be69da5799e975878fc77533ef4309c84
Prachi <prachi.swaroop@mabel.jewelry>
2023-02-28 10:58:28 +0530

    working login

--- src/auth/authentication_user.py
+++ src/auth/authentication_user.py
@@ -1,24 +1,53 @@
 import jwt
+from datetime import timedelta
+from typing import List
 from fastapi import Depends, HTTPException, status
 from fastapi.security import (
     OAuth2PasswordBearer,
     SecurityScopes,
 )
 from users.crud.users import MongoDBUserDatabase
-from users.models.users import UserInModel
+from users.models.users import UserInModel, UserModel
 from jwt import PyJWTError
 from pydantic import ValidationError
-
-from .jwt_handler import TokenData, pwd_context
+from fastapi.security import (
+    OAuth2PasswordBearer,
+    OAuth2PasswordRequestForm,
+    SecurityScopes,
+)
+from .jwt_handler import TokenData, pwd_context, create_access_token
 import os
 
 ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 129600
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/login")
 
 
 def verify_password(plain_password, hashed_password):
     data = pwd_context.verify(plain_password, hashed_password)
     return data
+
+
+async def authenticate(credentials: OAuth2PasswordRequestForm, enable_user: bool):
+    user_db = MongoDBUserDatabase(UserInModel)
+    username_hash = await user_db.get_username_hash(username=credentials.username.lower())
+    credentials.username = username_hash
+    credentials.password = credentials.password + "" + username_hash
+
+    if enable_user == True:
+        response = await user_db.enable_one(username=username_hash)
+        if response["internal_response_code"] != 0:
+            return False, False
+
+    user = await user_db.find_by_username(credentials.username)
+    if not user:
+        return False, False
+    if not verify_password(credentials.password, user.hashed_password):
+        return False, False
+
+    scopes = await get_scope_list(user=user)
+
+    return user, scopes
 
 async def get_current_user(
     security_scopes: SecurityScopes, token: str = Depends(oauth2_scheme)
@@ -107,3 +136,31 @@ async def get_scope_list(
         ]
 
     return scopes
+
+async def get_user_and_scope(
+    current_user: UserModel
+):
+    user_db = MongoDBUserDatabase(UserInModel)
+    user = await user_db.find_by_username(current_user.username)
+
+    if not user:
+        return False
+
+    scopes = await get_scope_list(user=user)
+    
+    return scopes, user
+
+async def get_token(
+    user: UserModel,
+    scopes: List
+):
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={
+            "username": user.username,
+            "mobile_number": user.mobile_number,
+            "scopes": scopes,
+        },
+            expires_delta=access_token_expires,
+    )
+    return access_token
